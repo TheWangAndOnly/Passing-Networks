58444c5dd56fcebd5cb5ec4ce62f63bc31ad9404
TheWangAndOnly <80552414+TheWangAndOnly@users.noreply.github.com>
2022-02-24 20:50:42 +0100

    get the passes correctly

--- Passing-Networks/statsbomb_data.py
+++ Passing-Networks/statsbomb_data.py
@@ -1,15 +1,15 @@
 from statsbombpy import sb
 import pandas as pd
 
-def get_competitions():
-    pass
-
 def get_matches(season_id=None, competition_id=None):
     return sb.matches(season_id, competition_id)
 
 def get_events(match_id=None):
-    return sb.events(match_id)
     
+    events = sb.events(match_id)
+    events = events[['id','team','player','position','pass_recipient','minute',
+                    'type', 'location', 'pass_end_location', 'pass_outcome',
+                   ]]
+    events = events[events['type'] == 'Pass']
     
-if __name__ == '__main__':
-    pass
+    return events
